6a7139f7cc8e1db209e76fa6db58cca5c7a9c6c4
Peter Waher <peterwaher@hotmail.com>
2023-06-26 14:55:58 -0400

    Finalize script

---
+++ TAG.Simulator/ObjectModel/MetaData/FinalizeScript.cs
@@ -0,0 +1,82 @@
+﻿using System.Threading.Tasks;
+using System.Xml;
+using Waher.Script;
+
+namespace TAG.Simulator.ObjectModel.MetaData
+{
+	/// <summary>
+	/// Executes script at the end of the simulation, allowing for persistance and
+	/// cleaning of states.
+	/// </summary>
+	public class FinalizeScript : SimulationNode
+	{
+		private string script;
+		private Expression expression;
+
+		/// <summary>
+		/// Executes model script, allowing for definitions that will be available across events.
+		/// </summary>
+		/// <param name="Parent">Parent node</param>
+		/// <param name="Model">Model in which the node is defined.</param>
+		public FinalizeScript(ISimulationNode Parent, Model Model)
+			: base(Parent, Model)
+		{
+		}
+
+		/// <summary>
+		/// Script string
+		/// </summary>
+		public string Script => this.script;
+
+		/// <summary>
+		/// Parsed expression
+		/// </summary>
+		public Expression Expression => this.expression;
+
+		/// <summary>
+		/// Local name of XML element defining contents of class.
+		/// </summary>
+		public override string LocalName => "FinalizeScript";
+
+		/// <summary>
+		/// Creates a new instance of the node.
+		/// </summary>
+		/// <param name="Parent">Parent node.</param>
+		/// <param name="Model">Model in which the node is defined.</param>
+		/// <returns>New instance</returns>
+		public override ISimulationNode Create(ISimulationNode Parent, Model Model)
+		{
+			return new FinalizeScript(Parent, Model);
+		}
+
+		/// <summary>
+		/// Sets properties and attributes of class in accordance with XML definition.
+		/// </summary>
+		/// <param name="Definition">XML definition</param>
+		public override Task FromXml(XmlElement Definition)
+		{
+			this.script = Values.Script.RemoveIndent(Definition.InnerText);
+			this.expression = new Expression(this.script);
+
+			return Task.CompletedTask;
+		}
+
+		/// <summary>
+		/// Initialized the node before simulation.
+		/// </summary>
+		public override async Task Initialize()
+		{
+			await this.expression.EvaluateAsync(this.Model.Variables);
+			await base.Initialize();
+		}
+
+		/// <summary>
+		/// Initialized the node before simulation.
+		/// </summary>
+		public override async Task Finalize()
+		{
+			await this.expression.EvaluateAsync(this.Model.Variables);
+			await base.Initialize();
+		}
+	}
+}
